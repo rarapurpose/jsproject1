887f13cb05cebeeb7dfa2f1ab11d162cbcc7531d
rarapurpose <129743144+rarapurpose@users.noreply.github.com>
2023-06-12 19:50:22 +0900

    weather check

--- js/weather.js
+++ js/weather.js
@@ -3,8 +3,15 @@ const API_KEY = "6901342141ea22aa8a8d08183b629c77";
 function onGeoOk(position) {
   const lat = position.coords.latitude;
   const lon = position.coords.longitude;
-  const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}`;
-  fetch(url);  
+  const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`;
+  fetch(url)  //js로 url을 불러온다
+  .then(response => response.json())
+  .then((data) => {
+    const weather = document.querySelector("#weather span:first-child");
+    const city = document.querySelector("#weather span:last-child")
+    city.innerText = data.name;
+    weather.innerText = `${data.weather[0].main} / ${data.main.temp}`;
+  });  
 }
 function onGeoError() {
   alert("Can't find you. No weather for you.");
