537aab30b29e164d6786b84e85449fea5880b9e4
rarapurpose <129743144+rarapurpose@users.noreply.github.com>
2023-06-12 19:24:41 +0900

    weather API

--- js/weather.js
+++ js/weather.js
@@ -1,7 +1,10 @@
+const API_KEY = "6901342141ea22aa8a8d08183b629c77";
+
 function onGeoOk(position) {
   const lat = position.coords.latitude;
-  const lng = position.coords.longitude;
-  console.log("Live it", lat, lng);
+  const lon = position.coords.longitude;
+  const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}`;
+  fetch(url);  
 }
 function onGeoError() {
   alert("Can't find you. No weather for you.");
