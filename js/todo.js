3fa0deb519c5f10a60a2ab43ceb28f2426d81b9e
rarapurpose <129743144+rarapurpose@users.noreply.github.com>
2023-06-12 17:50:54 +0900

    Update todo.js

--- js/todo.js
+++ js/todo.js
@@ -12,15 +12,16 @@ function saveToDos(){
 
 function deleteToDo(event){
   const li = event.target.parentElement;
-  console.log(li.id);
-  li.remove;
-}
+  li.remove();
+  toDos = toDos.filter((toDo) => toDo.id !== parseInt(li.id)); 
+  saveToDos();
+}   
 
 function paintToDo(newTodo){
   const li = document.createElement("li");
   li.id = newTodo.id;
   const span = document.createElement("span");
-  span.innerText = newTodo.text; 
+  span.innerText = newTodo.text;
   const button = document.createElement("button");
   button.innerText = "❌";
   button.addEventListener("click", deleteToDo);    
@@ -48,7 +49,7 @@ const savedToDos = localStorage.getItem(TODOS_KEY);
 
 if (savedToDos !== null) {
   const parsedToDos = JSON.parse(savedToDos);
-  toDos = parsedToDos;
+  toDos = parsedToDos;  
   parsedToDos.forEach(paintToDo);
 }
 
