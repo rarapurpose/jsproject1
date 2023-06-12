abe454e621619e06ea68d79bf697975de52d427a
rarapurpose <129743144+rarapurpose@users.noreply.github.com>
2023-06-12 20:15:44 +0900

    Update weather.js

--- js/weather.js
+++ js/weather.js
@@ -4,7 +4,7 @@ function onGeoOk(position) {
   const lat = position.coords.latitude;
   const lon = position.coords.longitude;
   const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`;
-  fetch(url)  //js로 url을 불러온다
+  fetch(url)  
   .then(response => response.json())
   .then((data) => {
     const weather = document.querySelector("#weather span:first-child");
