9f570940c524031ba4588c33308feb9c8d68c99d
rarapurpose <129743144+rarapurpose@users.noreply.github.com>
2023-06-13 20:51:23 +0900

    css, size

--- js/background.js
+++ js/background.js
@@ -8,11 +8,3 @@ const bgImage = document.createElement("img");
 bgImage.src = `img/${chosenImage}`;
 
 document.body.appendChild(bgImage);
-
-
-//Math..random()으로 나온 숫자에 images Array의 길이 값을 곱한 숫자를 내림 
-//document.createElement("img")만으로 html img를 만든다!
-// bgImage.src = `img/${chosenImage}`; 
-//html 의 <img src="img/"pic4.jpg/> 랑 같음
-//bgImage를 body 내부에 추가
-//Element 는 HTML tag들을 가져올 수 있다
--- js/quotes.js
+++ js/quotes.js
@@ -1,36 +1,36 @@
 const quotes = [
   {
     quote: "The biggest adventure you can take is to live the life of your dreams.",
-    author: "Oprah Winfrey",
+    author: "-Oprah Winfrey-",
   },
   {
     quote: "Life is either a daring adventure or nothing at all.",
-    author: "Helen Keller",
+    author: "-Helen Keller-",
   },
   {
     quote: "In every moment, the world is changing into something else.",
-    author: "Haruki Murakami",
+    author: "-Haruki Murakami-",
 
   },
   {
     quote: "There is nothing as mysterious as a fact clearly described.",
-    author: "Ryunosuke Akutagawa",
+    author: "-Ryunosuke Akutagawa-",
   },
   {
     quote: "There is nothing as mysterious as a fact clearly described.",
-    author: "Ryunosuke Akutagawa",
+    author: "-Ryunosuke Akutagawa-",
   },
   {
     quote: "The world is full of good and evil, but words are capable of changing the world.",
-    author: "Natsume Soseki",
+    author: "-Natsume Soseki-",
   },
   {
     quote: "The courage to continue chasing your dreams becomes the power to change the future.",
-    author: "Forrest Gump",
+    author: "-Forrest Gump-",
   },
   {
     quote: "The only way to do great work is to love what you do.",
-    author: "Steve Jobs",
+    author: "-Steve Jobs-",
   },
   {
     quote: "Life is like a box of chocolates; you never know what you're gonna get.",
@@ -38,11 +38,11 @@ const quotes = [
   },
   {
     quote: "The harder the times, the more you need to face forward.",
-    author: "Akio Morita ",
+    author: "-Akio Morita- ",
   },
   {
     quote: "You set your own limits, and you can surpass them by constantly challenging yourself.",
-    author: "Hiroshi Y",
+    author: "-Hiroshi Y-",
   },
 ];
 
